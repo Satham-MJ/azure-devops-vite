054785359b59e5e10f955ec9301fcb782777d7e4
Satham Ussain <satham.edu@gmail.com>
2024-12-26 22:46:19 +0530

    Somechange to understand Docker Working

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Starter of Azure DevOps</h1>
+      <h1>Starter of Azure DevOps (I am running from DockerFile)</h1>
     </>
   );
 }
